356ac635ee5ec5a28e53aa97237966d1aa50941e
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-10-15 21:54:54 +0200

    Basic functions working

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -2,50 +2,40 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import { v4 as uuidv4 } from 'uuid';
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	if (vscode.env.uiKind === vscode.UIKind.Web) {
-		console.log("'vscode-ulid-generator' is active in an web extension host");
+		console.log("'vscode-guid-generator' is active in an web extension host");
 	} else if (vscode.env.uiKind === vscode.UIKind.Desktop) {
-		console.log("'vscode-ulid-generator' is active in an desktop extension host");
+		console.log("'vscode-guid-generator' is active in an desktop extension host");
 	} else {
-		console.log("'vscode-ulid-generator' is active in an unknown extension host");
+		console.log("'vscode-guid-generator' is active in an unknown extension host");
 	}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	// For each cursor in the current editor window, generate a GUID and insert it
+	// This works for a single cursor, or when in multi-cursor mode
 	context.subscriptions.push(vscode.commands.registerCommand('vscode-guid-generator.insertGUID', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		if (vscode.env.uiKind === vscode.UIKind.Web) {
-			vscode.window.showInformationMessage("'vscode-ulid-generator' is active in an web extension host");
-		} else if (vscode.env.uiKind === vscode.UIKind.Desktop) {
-			vscode.window.showInformationMessage("'vscode-ulid-generator' is active in an desktop extension host");
-		} else {
-			vscode.window.showInformationMessage("'vscode-ulid-generator' is active in an unknown extension host");
+		let editor = vscode.window.activeTextEditor;
+        if (editor) {
+			editor.edit( edit => {
+				editor.selections.forEach( v => edit.replace( v, makeGuid() ) );
+			} );
 		}
 	}));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	// Generate and copy a single GUID to the clipboard
 	context.subscriptions.push(vscode.commands.registerCommand('vscode-guid-generator.copy', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		if (vscode.env.uiKind === vscode.UIKind.Web) {
-			vscode.window.showInformationMessage("'vscode-ulid-generator' is active in an web extension host");
-		} else if (vscode.env.uiKind === vscode.UIKind.Desktop) {
-			vscode.window.showInformationMessage("'vscode-ulid-generator' is active in an desktop extension host");
-		} else {
-			vscode.window.showInformationMessage("'vscode-ulid-generator' is active in an unknown extension host");
-		}
+		vscode.env.clipboard.writeText( makeGuid() );
+		vscode.window.showInformationMessage("'GUID copied to clipboard");
 	}));
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function makeGuid() : string {
+	return uuidv4();
+}
